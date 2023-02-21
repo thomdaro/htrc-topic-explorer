12de9c70b89520af21839e55918a0b3b991d76e1
thomdaro <31260388+thomdaro@users.noreply.github.com>
2023-02-21 14:30:03 -0500

    Update test_topicexplorer_lib_pdf.py
    Update paths properly this time

--- tests/test_topicexplorer_lib_pdf.py
+++ tests/test_topicexplorer_lib_pdf.py
@@ -21,23 +21,23 @@ class TestPdf(unittest.TestCase):
     @patch('topicexplorer.lib.pdf.subprocess.check_output')
     def test_convert_pdfminer(self, not_found_mock):
         not_found_mock.return_value = b''
-        out = topicexplorer.lib.pdf.convert("/opt/anaconda/lib/python3.6/site-packages/www/papers/aaai15-topic-explorer-demo.pdf")
+        out = topicexplorer.lib.pdf.convert("/opt/anaconda/lib/python3.6/site-packages/htrc-topic-explorer/www/papers/aaai15-topic-explorer-demo.pdf")
         self.assertGreater(len(out), 0)
     
     def test_convert_pdftotext(self):
-        out = topicexplorer.lib.pdf.convert("/opt/anaconda/lib/python3.6/site-packages/www/papers/aaai15-topic-explorer-demo.pdf")
+        out = topicexplorer.lib.pdf.convert("/opt/anaconda/lib/python3.6/site-packages/htrc-topic-explorer/www/papers/aaai15-topic-explorer-demo.pdf")
         self.assertGreater(len(out), 0)
 
     def test_main(self):
         output_dir = mkdtemp()
-        topicexplorer.lib.pdf.main("/opt/anaconda/lib/python3.6/site-packages/www/papers/aaai15-topic-explorer-demo.pdf",
+        topicexplorer.lib.pdf.main("/opt/anaconda/lib/python3.6/site-packages/htrc-topic-explorer/www/papers/aaai15-topic-explorer-demo.pdf",
             output_dir=output_dir)
         self.assertEqual(1, len(os.listdir(output_dir)))
         shutil.rmtree(output_dir)
 
     def test_convert_and_write(self):
         output_dir = mkdtemp()
-        topicexplorer.lib.pdf.convert_and_write("/opt/anaconda/lib/python3.6/site-packages/www/papers/aaai15-topic-explorer-demo.pdf",
+        topicexplorer.lib.pdf.convert_and_write("/opt/anaconda/lib/python3.6/site-packages/htrc-topic-explorer/www/papers/aaai15-topic-explorer-demo.pdf",
             output_dir=output_dir, overwrite=True, verbose=True)
         self.assertEqual(1, len(os.listdir(output_dir)))
         shutil.rmtree(output_dir)
